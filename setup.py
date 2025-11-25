ea81441cf8ea76348e860b078ae3a5b5e5ee33f4
Huss Elshamy <elshamyhussein@gmail.com>
2025-11-25 17:33:18 -0600

    Add package_data for tokens.yaml in setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
+    package_data={"poof_token_registry": ["tokens.yaml"]},
     install_requires=["pyyaml"],
 )
